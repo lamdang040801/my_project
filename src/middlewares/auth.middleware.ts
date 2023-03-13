7bf71d468c947be00a27f53cbb32d5b167679bd0
lamdang040801 <lam.dang040801@stechvn.org>
2023-03-13 23:57:18 +0700

    ok

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,25 @@
+import { verify } from "jsonwebtoken"
+import { Request, Response, NextFunction } from "express";
+
+export const auth = async (req: Request, res: Response, next: NextFunction) => {
+    const authString = req.headers['authentication'];
+    if(!authString) {
+        return res.status(401).json({
+            success: false,
+            message: "Chưa đăng nhập"
+        })
+    }
+    console.log(authString)
+    const token = (authString as string).split(' ')[1]
+    if(!token) {
+        return res.status(401).json({
+            success: false,
+            message: "Chưa đăng nhập"
+        })
+    }
+
+    const authData = verify(token, process.env.JWT as string);
+    req.body.authData = authData
+
+    next()
+}
